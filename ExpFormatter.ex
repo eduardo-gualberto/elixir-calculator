1b99892537a1020f599098983e81d1a80b1a2277
Eduardo Gualberto <gualberto.se@yahoo.com.br>
2023-06-06 13:53:17 -0300

    fixes a problem in which it was not possible to write redundant parens

--- ExpFormatter.ex
+++ ExpFormatter.ex
@@ -6,14 +6,16 @@ defmodule ExpFormatter do
   end
 
   defp _format_exp(exp) do
-    import String, only: [replace: 3, trim: 1]
-    replace(exp, ~r/\s+/, "")                                                        # strip spaces
-    |> replace(~r/\++/, "+")                                                         # multiple '+'s
-    |> replace(~r/(--)+/, "+")                                                       # canceling '-'s
-    |> replace(~r/(\+-)+/, "-")                                                      # '+-' equals '-'
-    |> replace(["+", "-", "*", "/", "^", "(", ")"], fn op -> " " <> op <> " " end)   # spaces around operators ("*" -> " * ")
-    |> replace(~r/\s+/, " ")                                                         # remove duplicate spaces
-    |> trim                                                                          # remove leading/trailing spaces
+    import String, only: [replace: 3, trim: 1, length: 1, slice: 3]
+    import Kernel, except: [length: 1]
+    replace(exp, ~r/\s+/, "")                                                                     # strip spaces
+    |> replace(~r/(\()+(([\+-]+)?[\da-zA-Z]+)(\))+/, fn m -> slice(m, 1, length(m) - 2) end)      # removes redundant parens
+    |> replace(~r/\++/, "+")                                                                      # multiple '+'s
+    |> replace(~r/(--)+/, "+")                                                                    # canceling '-'s
+    |> replace(~r/(\+-)+/, "-")                                                                   # '+-' equals '-'
+    |> replace(["+", "-", "*", "/", "^", "(", ")"], fn op -> " " <> op <> " " end)                # spaces around operators ("*" -> " * ")
+    |> replace(~r/\s+/, " ")                                                                      # remove duplicate spaces
+    |> trim                                                                                       # remove leading/trailing spaces
   end
 
 end
